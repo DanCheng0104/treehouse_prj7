2c1f3d67e21155153a8a4fcaf58f9deeaf14f859
DAN CHENG <dcheng0104@gmail.com>
2017-09-08 16:57:23 -0700

    finish the first section

--- js/app.js
+++ js/app.js
@@ -19,46 +19,47 @@ T.get('account/verify_credentials', { skip_status: true })
     // for details.
     paras.name = result.data.name;
     paras.screen_name = result.data.screen_name;
-    T.get('statuses/user_timeline', { screen_name: paras.screen_name , count: 5 })
+    paras.profile_image_url = result.data.profile_image_url;
+    paras.friends_count = result.data.friends_count;
+    paras.followers_count = result.data.followers_count;
+
+        		// t_content['profile_image_url'] = t['user']['profile_image_url'];
+    T.get('statuses/user_timeline', { screen_name: paras.screen_name , count: 5})
       .catch(function (err) {
     console.log('caught error', err.stack)
   })
   .then(function (result){
     	paras.tweets=[];
+    	// console.log(result.data);
     	result.data.forEach(t=>{
     		const t_content = {};
+
+    		t_content['created_at'] = t['created_at'];
     		t_content['text'] = t['text'];
     		t_content['retweet_count'] = t['retweet_count'];
     		t_content['favorite_count'] = t['favorite_count'];
     		paras.tweets.push(t_content);
 
     	})
-    	   console.log(paras);
+    	T.get('friends/list',{count:1})
+    	.catch(function (err) {
+    		console.log('caught error', err.stack)
+  		})
+  		.then(function(result){
+  			console.log(result.data.users);
+  			//name,screen_name,profile_background_image_url
+  		});
+	 //    app.get('/',(req,res)=>{
+		// res.render('index',paras);
+	 //   });
   });
 
- //    T.get('statuses/user_timeline', { screen_name: paras.screen_name , count: 5 }, function(err, data, response) {
- //    	paras.tweets=[];
- //    	data.forEach(t=>{
- //    		const t_content = {};
- //    		t_content['text'] = t['text'];
- //    		t_content['retweet_count'] = t['retweet_count'];
- //    		t_content['favorite_count'] = t['favorite_count'];
- //    		paras.tweets.push(t_content);
-
- //    	})
- //    	   console.log(paras);
-	// });
   })
 
 
-
-
-// app.get('/',(req,res)=>{
-//     res.render('index');
-// });
-
-
+	 //    app.get('/',(req,res)=>{
+		// res.render('index',{ screen_name: paras.screen_name});
+	 //   });
 app.listen(3000,()=>{
 	console.log('the app is running');
 });
-
