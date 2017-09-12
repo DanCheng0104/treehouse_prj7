5c157c05a1c91fb83ed6e4e4cc73fd2a5c5a1bdb
DAN CHENG <dcheng0104@gmail.com>
2017-09-11 17:01:46 -0700

    add message to convo

--- js/app.js
+++ js/app.js
@@ -23,57 +23,113 @@ T.get('account/verify_credentials', { skip_status: true })
     paras.friends_count = result.data.friends_count;
     paras.followers_count = result.data.followers_count;
 
-        		// t_content['profile_image_url'] = t['user']['profile_image_url'];
+          // t_content['profile_image_url'] = t['user']['profile_image_url'];
     T.get('statuses/user_timeline', { screen_name: paras.screen_name , count: 5})
       .catch(function (err) {
     console.log('caught error', err.stack)
   })
   .then(function (result){
-    	paras.tweets=[];
-    	// console.log(result.data);
-    	result.data.forEach(t=>{
-    		const t_content = {};
+     paras.tweets=[];
+     // console.log(result.data);
+     result.data.forEach(t=>{
+      const t_content = {};
 
-    		t_content['created_at'] = t['created_at'];
-    		t_content['text'] = t['text'];
-    		t_content['retweet_count'] = t['retweet_count'];
-    		t_content['favorite_count'] = t['favorite_count'];
-    		paras.tweets.push(t_content);
+      t_content['created_at'] = t['created_at'];
+      t_content['text'] = t['text'];
+      t_content['retweet_count'] = t['retweet_count'];
+      t_content['favorite_count'] = t['favorite_count'];
+      paras.tweets.push(t_content);
 
-    	})
-    	T.get('friends/list',{count:5})
-    	.catch(function (err) {
-    		console.log('caught error', err.stack)
-  		})
-  		.then(function(result){
-        paras.users=[];
-        result.data.users.forEach(u=>{
-          const user = {};
-          user['name']=u.name;
-          user['screen_name']=u.screen_name;
-          user['profile_image_url']=u.profile_image_url;
-          paras.users.push(user);
-        });
+     })
+     T.get('friends/list',{count:5})
+     .catch(function (err) {
+      console.log('caught error', err.stack)
+    })
+    .then(function(result){
+         paras.users=[];
+         result.data.users.forEach(u=>{
+           const user = {};
+           user['name']=u.name;
+           user['screen_name']=u.screen_name;
+           user['profile_image_url']=u.profile_image_url;
+           paras.users.push(user);
+         });
 
-        T.get('direct_messages',{count:5})
-        .catch(function (err) {
-          console.log('caught error', err.stack)
-        })
-        .then(function(result){
-          console.log(result.data);
-        });
-  		});
-	 //    app.get('/',(req,res)=>{
-		// res.render('index',paras);
-	 //   });
+         T.get('direct_messages',{count:5})
+         .catch(function (err) {
+           console.log('caught error', err.stack)
+         })
+         .then(function(result){
+           paras.messages=[];
+           result.data.forEach(m=>{
+            const msg={};
+            msg['text'] = m.text;
+            msg['sender_screen_name'] = m.sender_screen_name;
+            msg['profile_image_url'] = m.sender.profile_image_url;
+            msg['created_at'] = m.created_at;
+            msg['recipient_screen_name'] = m.recipient_screen_name;
+            msg['received'] = 1;
+            paras.messages.push(msg);
+           });
+            T.get('direct_messages/sent',{count:5})
+            .catch(function (err) {
+              console.log('caught error', err.stack)
+            })
+            .then(function(result){
+              result.data.forEach(m=>{
+               const msg={};
+               msg['text'] = m.text;
+               msg['sender_screen_name'] = m.sender_screen_name;
+               msg['profile_image_url'] = m.sender.profile_image_url;
+               msg['created_at'] = m.created_at;
+               msg['recipient_screen_name'] = m.recipient_screen_name;
+               msg['received'] = 0;
+               paras.messages.push(msg);
+              });
+              createConversation(paras);
+        
+            });          
+           
+         });
+    });
+  //    app.get('/',(req,res)=>{
+  // res.render('index',paras);
+  //   });
   });
 
-  })
+  });
+
+  function sortMsg(a,b){
+   let comparison = 0;
+   return comparison = (a.created_at > b.created_at)?1:-1;
+   
+  }
+  function createConversation(paras){
+    let conversations = {};
+    paras.messages.forEach(m=>{
+     const send_screen_name = m['sender_screen_name'];
+     if ((m['received'] ==1)&&(send_screen_name !== paras.screen_name) && (!conversations.hasOwnProperty(send_screen_name))) {
+        conversations[send_screen_name]=[]
+        conversations[send_screen_name].push(m);
+     }
+    });
+    paras.messages.forEach(m=>{
+     const recipient_screen_name = m['recipient_screen_name'];
+     if (m['received'] ==0) {conversations[recipient_screen_name].push(m);}
+    });
+
+    for (let key in conversations){
+      conversations[key].sort(sortMsg);
+    }
+
+    paras.conversations = conversations;
+    console.log(paras.conversations);
+  }
 
 
-	 //    app.get('/',(req,res)=>{
-		// res.render('index',{ screen_name: paras.screen_name});
-	 //   });
+  //    app.get('/',(req,res)=>{
+  // res.render('index',{ screen_name: paras.screen_name});
+  //   });
 app.listen(3000,()=>{
-	console.log('the app is running');
+ console.log('the app is running');
 });
